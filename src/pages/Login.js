f0605d8c986d5a8ede2ade087f7a6ac491458303
심민성 <75308777+owl1753@users.noreply.github.com>
2022-03-31 01:02:27 +0900

    20220331

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,92 @@
+import styled from "styled-components";
+
+const LoginWrapper = styled.div`
+    &{
+        color: #E8E8E8;
+        font-size: 20px;
+    }
+    .LoginBox{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        background-color: black;
+        padding-top: 2em;
+        padding-left: 2em;
+        padding-right: 2em;
+        border: 4px solid #E8E8E8;
+    }
+    .LoginBox>div:first-child{
+        margin-bottom: 2em;
+    }
+    .LoginBox>div:not(:last-child)>div{
+        padding-left: 5px;
+        padding-bottom: 5px;
+    }
+    .LoginBox>div>input{
+        border: 0px;
+        width: 300px;
+        height: 30px;
+        font-size: 22px;
+    }
+    .LoginBox>div>input:focus{
+        outline: none;
+    }
+    .LoginBox>div:last-child{
+        display: flex;
+        justify-content: flex-end;
+    }
+    .LoginBox>div:last-child{
+        display: flex;
+        justify-content: center;
+    }
+    .LoginBox>div:last-child>div{
+        display: flex;
+        padding-top: 1.5em;
+        padding-bottom: 1.5em;
+    }
+    .LoginBox>div:last-child>div>button{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100px;
+        height: 40px;
+        font-family: 'OTWelcomeBA';
+        font-size: 16px;
+        border: 3px solid #E8E8E8;
+        background-color: black;
+        color: #E8E8E8;
+    }
+    .LoginBox>div:last-child>div>button:first-child{
+        margin-right: 2em;
+    }
+    .LoginBox>div:last-child>div>button:active{
+        background-color: #E8E8E8;
+        color: black;
+    }
+`
+
 const Login = (props) => {
     return (
-        <>
-            <div>야스</div>
-        </>
+        
+        <LoginWrapper>
+            <div className="LoginBox">
+                <div>
+                    <div>아이디</div>
+                    <input type="text" spellCheck={ false }/>
+                </div>
+                <div>   
+                    <div>비밀번호</div>
+                    <input type="password"/>
+                </div>
+                <div>
+                    <div>
+                        <button>로그인</button>
+                        <button>회원가입</button>
+                    </div>
+                </div>
+            </div>
+        </LoginWrapper>
     )
 }
 
